4a2b7372c2b340e0debb6ff9daaa969930960dd1
Wale <toyiselateef2@gmail.com>
2025-09-17 05:14:54 -0400

    fix pdf

--- MfgDocs.Api/obj/Debug/net8.0/MfgDocs.Api.AssemblyInfo.cs
+++ MfgDocs.Api/obj/Debug/net8.0/MfgDocs.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MfgDocs.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9294f45a462054676928ba17d5cbd99a778736d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e4a1805b37c765305d9ad8d5f2888706542adcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MfgDocs.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MfgDocs.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
